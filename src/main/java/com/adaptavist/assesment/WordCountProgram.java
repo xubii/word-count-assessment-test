aa26f3e42516e6230561d40f166d3e3f16036897
Muhammad Zohaib Baig <xubii@Muhammads-MacBook-Pro.local>
2023-11-03 15:21:53 +0000

    Complete word count program with test cases

---
+++ src/main/java/com/adaptavist/assesment/WordCountProgram.java
@@ -0,0 +1,52 @@
+package main.java.com.adaptavist.assesment;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class WordCountProgram {
+    private Map<String, Integer> wordCountMap;
+
+    public WordCountProgram() {
+        wordCountMap = new HashMap<>();
+    }
+
+    public void readFromInputFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String fileData;
+            while ((fileData = reader.readLine()) != null) {
+                String[] words = fileData.split("\\s+");
+                for (String word : words) {
+                    word = word.toLowerCase().replaceAll("[^a-zA-Z]", "");
+                    if (!word.isEmpty()) {
+                        wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Map.Entry<String, Integer>> sortByMostOccurrences() {
+        return wordCountMap.entrySet().stream()
+                .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+                .collect(Collectors.toList());
+
+    }
+
+    public void printWordCount(List<Map.Entry<String, Integer>> sortedMapList){
+        for (Map.Entry<String, Integer> entry : sortedMapList) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "<Please Provide File Path>";
+//        String filePath = "/Users/zohaib/Downloads/wordCount.txt";
+        WordCountProgram wordCounter = new WordCountProgram();
+        wordCounter.readFromInputFile(filePath);
+        wordCounter.printWordCount(wordCounter.sortByMostOccurrences());
+    }
+}
+
---
+++ src/test/java/com/adaptavist/assessment/WordCountProgramTest.java
@@ -0,0 +1,44 @@
+package test.java.com.adaptavist.assessment;
+
+import main.java.com.adaptavist.assesment.WordCountProgram;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordCountProgramTest {
+
+    private WordCountProgram wordCounter;
+    private ByteArrayOutputStream outContent;
+    private PrintStream originalOut;
+
+    @BeforeEach
+    public void setUp() {
+        wordCounter = new WordCountProgram();
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void verifyCountOfOccurrencesWord() {
+        String filePath = "<File_Path>";
+        wordCounter.readFromInputFile(filePath); // Replace with your input file
+        wordCounter.printWordCount(wordCounter.sortByMostOccurrences());
+
+        String[] expectedOutput = new String[]{"et: 6", "in: 5", "eget: 5"};
+        String[] result = outContent.toString().split("\\n"); // split by new lines
+
+        assertAll("",
+                () -> assertEquals(expectedOutput[0], result[0]),
+                () -> assertEquals(expectedOutput[1], result[1]),
+                () -> assertEquals(expectedOutput[2], result[2])
+        );
+
+    }
+
+}
